695b649652da386ce50f882e654573a2007e243f
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-12-26 08:50:03 -0600

    Create LISDIGIT.cpp

---
+++ LISDIGIT.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        int len; scanf("%d", &len);
+        while(len--){int d; scanf("%d", &d); printf("%d", d);}
+        puts("");
+    }
+
+    return 0;
+}
