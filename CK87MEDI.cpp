35691e51726d4e99f3c2135a93ea6ea20a0dfaeb
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-03-18 10:49:23 -0500

    Create CK87MEDI.cpp

---
+++ CK87MEDI.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, k; scanf("%ld %ld", &n, &k);
+        std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+        sort(a.begin(), a.end());
+        printf("%ld\n", a[(n + k) / 2]);
+    }
+
+    return 0;
+}
