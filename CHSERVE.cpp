2bfb1f66d84f7047716cf65987e60cc750100a45
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-12-04 12:35:13 -0600

    Create CHSERVE.cpp

---
+++ CHSERVE.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long x, y, z; scanf("%ld %ld %ld", &x, &y, &z);
+        puts(((x + y) / z) % 2 ? "COOK" : "CHEF");
+    }
+
+    return 0;
+}
