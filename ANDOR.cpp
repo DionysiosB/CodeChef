d94cb19aa355fc3561bdc8e5a420c35a3f1a2af4
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-12-20 19:09:09 -0600

    Create ANDOR.cpp

---
+++ ANDOR.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+typedef long long ll;
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        ll x; scanf("%lld", &x);
+        printf("0 %lld\n", x);
+    }
+}
