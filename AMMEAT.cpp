31a8c4927c671c20ca4936cb3a81e53091f02338
Dionysios Barmpoutis <barmpoutis@gmail.com>
2022-01-09 07:46:04 -0600

    Update AMMEAT.cpp

--- AMMEAT.cpp
+++ AMMEAT.cpp
@@ -1,27 +1,18 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
 #include <algorithm>
-using namespace std;
+typedef long long ll;
 
 int main(){
 
-    size_t numPlates = 0, usedPlates = 0;
-    unsigned long long minMeatballs = 0, numMeatballs =0, total = 0;
-
-    int numCases; scanf("%d\n",&numCases);
-    while(numCases--){
-        scanf("%zd %llu\n", &numPlates, &minMeatballs);
-        unsigned long long *numMeatballs = new unsigned long long[numPlates]; 
-        for(size_t k = 0; k < numPlates; k++){scanf("%llu",numMeatballs+k);}
-        sort(numMeatballs, numMeatballs + numPlates);
-
-        usedPlates = 0; total = 0;
-        for(int k = numPlates - 1; k >= 0; k--){
-            total += numMeatballs[k];
-            usedPlates++;
-            if(total >= minMeatballs){break;}
-        }
-        if(total >= minMeatballs){printf("%zd\n",usedPlates);}
-        else{printf("-1\n");}
+    ll t; scanf("%lld", &t);
+    while(t--){
+        ll n, m; scanf("%lld %lld", &n, &m);
+        std::vector<ll> a(n); for(ll p = 0; p < n; p++){scanf("%lld", &a[p]);}
+        sort(a.rbegin(), a.rend());
+        ll s(0), res(0);
+        for(ll p = 0; p < n; p++){s += a[p]; if(s >= m){res = p + 1; break;}}
+        printf("%lld\n", (s >= m) ? res : -1);
     }
 
     return 0;
