9ccc35523a6624acbdbfc1c3fb5177986fc4ef10
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-03-08 20:13:57 -0600

    Create COMPCAND.cpp

---
+++ COMPCAND.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, k; scanf("%ld %ld", &n, &k);
+        printf("%ld\n", (n % k) ? (-1) : (n / k));
+    }
+
+}
