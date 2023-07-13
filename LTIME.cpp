93a930a24c9fbf1db7ba76fe05a0e2aadbe7ae9b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-07-13 07:02:34 -0500

    Create LTIME.cpp

---
+++ LTIME.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long x; scanf("%ld", &x);
+        puts((1 <= x && x <= 4) ? "YES" : "NO");
+
+    }   
+
+}
