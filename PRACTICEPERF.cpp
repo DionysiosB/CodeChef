4afa23e20c5880b0ffd35f95df9bee060d561578
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-08-03 07:04:04 -0500

    Create PRACTICEPERF.cpp

---
+++ PRACTICEPERF.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main(){
+
+    long p1, p2, p3, p4; scanf("%ld %ld %ld %ld", &p1, &p2, &p3, &p4);
+    printf("%d\n", (p1 >= 10) + (p2 >= 10) + (p3 >= 10) + (p4 >= 10));
+
+}
