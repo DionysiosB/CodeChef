2177eeb12792268d17e4fb6ba195ce8dd5558e4d
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-08-13 08:47:31 -0500

    Create MINPIZZA.cpp

---
+++ MINPIZZA.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, x; scanf("%ld %ld", &n, &x);
+        printf("%ld\n", (n * x + 3) / 4);
+    }   
+
+}
