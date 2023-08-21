e116b9ba20a728dd52897df35e06719ad544f6dd
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-08-21 06:49:10 -0500

    Create JCOINS.cpp

---
+++ JCOINS.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long x, y; scanf("%ld %ld", &x, &y);
+        printf("%ld\n", 10 * x + 5 * y);
+    }   
+
+}
