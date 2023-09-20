d3031f21c6b2ca235428f1bc217c02b626ca2c46
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-09-20 06:28:19 -0500

    Create MANGOES.cpp

---
+++ MANGOES.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long x, y, z; scanf("%ld %ld %ld", &x, &y, &z);
+        printf("%ld\n", (z - y) / x);
+    }   
+
+}
