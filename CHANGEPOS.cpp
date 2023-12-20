98d058fcc8cda55479e5e76f2e1427aa46a4880c
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-12-20 06:58:36 -0600

    Create CHANGEPOS.cpp

---
+++ CHANGEPOS.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long a, b, c, d; scanf("%ld %ld %ld %ld", &a, &b, &c, &d);
+        printf("%d\n", 1 + (a == c || b == d));
+    }   
+
+}
