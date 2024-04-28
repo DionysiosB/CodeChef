6de65360422b0dbd770b47712e10ae505158aff7
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-04-28 06:22:09 -0500

    Create CHEFPAGES.cpp

---
+++ CHEFPAGES.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+
+    int a, b; scanf("%d %d", &a, &b);
+    if(!a){puts("https://www.codechef.com/practice");}
+    else if(!b){puts("https://www.codechef.com/contests");}
+    else{puts("https://discuss.codechef.com");}
+
+}
