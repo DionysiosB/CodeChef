8ac7ffcc5addbc66a2118b823857f6d057eb3ebd
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-09-06 06:28:35 -0500

    Create EXPENSES.cpp

---
+++ EXPENSES.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, x; scanf("%ld %ld", &n, &x);
+        printf("%ld\n", 1L << (x - n));
+    }   
+
+}
