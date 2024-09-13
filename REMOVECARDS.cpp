55f4a12c84a69bd25020ccc6800f99c41dee5718
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-09-13 07:36:57 -0500

    Create REMOVECARDS.cpp

---
+++ REMOVECARDS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        std::map<long, long> m; long mx(0);
+        for(long p = 0; p < n; p++){
+            long x; scanf("%ld", &x);
+            ++m[x];
+            mx = (mx > m[x]) ? mx : m[x];
+        }
+
+        printf("%ld\n", n - mx);
+    }
+
+}
