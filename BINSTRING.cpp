ca989c8260271cd42353db3965a248a700b593de
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-12-22 06:45:07 -0600

    Create BINSTRING.cpp

---
+++ BINSTRING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    long t; std::cin >> t;
+    while(t--){
+        long n; std::cin >> n;
+        std::string s; std::cin >> s;
+        long cnt(1);
+        for(long p = 1; p < s.size(); p++){cnt += (s[p - 1] != s[p]);}
+        std::cout << cnt << std::endl;
+    }
+
+}
