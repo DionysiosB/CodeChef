045a6b6f36c8dfe6fe2b0b790db727c0ca6e4b2c
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-02-28 16:09:06 -0500

    Create PARLIAMENT.cpp

---
+++ PARLIAMENT.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, x; scanf("%ld %ld", &n, &x);
+        puts(x >= (n - x) ? "YES" : "NO");
+    }
+
+}
