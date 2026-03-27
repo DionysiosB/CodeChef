7f21636e4c8fd330d6897b498578a5a559452649
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-03-27 06:14:01 -0400

    Create CS2023_GYM.cpp

---
+++ CS2023_GYM.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+    
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, m; scanf("%ld %ld", &n, &m);
+        printf("%ld\n", (m + 2) / 2);
+    }
+}
